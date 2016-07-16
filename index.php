525e1b03513df10600963028a29d229c98079a98
Robert Engelbert <Robe32002@yahoo.com>
2016-07-16 10:04:09 -0600

    Start of my skoolie page

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 	<html lang="en">
 	<head>
-		<title></title>
+		<title>Skoolies rock</title>
 	</head>
 	<body>
-		<h1>Hello world!</h1>
+		<h1>Skoolies</h1>
+		<p>My skoolie is a 2001 International 3000, It's 38ft long. I bought it in Feburary 2015, and started converting it immediately. I finished in Feburauy 2016 and now have a awesome home.</p>
+		<img src="" alt="" />
+		<img src="images/finishedskooliebus.jpg" alt="Picture of my bus finished" />
 	</body>
 </html>
