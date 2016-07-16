5a806a1062898370afd9a651148ddebd96d84a01
Robert Engelbert <Robe32002@yahoo.com>
2016-07-16 10:51:28 -0600

    Added some css

--- index.php
+++ index.php
@@ -2,11 +2,16 @@
 	<html lang="en">
 	<head>
 		<title>Skoolies rock</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
-		<h1>Skoolies</h1>
+		<header class="head">
+			<h1>Skoolies</h1>
+		</header>
 		<p>My skoolie is a 2001 International 3000, It's 38ft long. I bought it in Feburary 2015, and started converting it immediately. I finished in Feburauy 2016 and now have a awesome home.</p>
 		<img src="" alt="" />
-		<img src="images/finishedskooliebus.jpg" alt="Picture of my bus finished" />
+		<div id="bus">
+			<img src="images/finishedskooliebus.jpg" alt="Picture of my bus finished" />
+		</div>
 	</body>
 </html>
