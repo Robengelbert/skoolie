9ed14392efd6eca647606c0534cd602802e048ed
Robert Engelbert <Robe32002@yahoo.com>
2016-09-25 09:37:09 -0600

    Added links to bootstrap and jquery

--- index.php
+++ index.php
@@ -2,7 +2,18 @@
 	<html lang="en">
 	<head>
 		<title>Skoolies rock</title>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!--		custom CSS-->
 		<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+<!--		JQuery-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</head>
 	<body>
 		<header class="head">
@@ -13,5 +24,6 @@
 		<div id="bus">
 			<img src="images/finishedskooliebus.jpg" alt="Picture of my bus finished" />
 		</div>
+
 	</body>
 </html>
