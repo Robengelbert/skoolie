d1c141ba7677f77fa036595156895bed02efcfca
Robert Engelbert <Robe32002@yahoo.com>
2016-09-25 14:55:56 -0600

    Fixed format for first youtube video.

--- index.php
+++ index.php
@@ -1,32 +1,56 @@
 <!DOCTYPE html>
-	<html lang="en">
+<html lang="en">
 	<head>
 		<title>Skoolies rock</title>
 
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<!--		custom CSS-->
-		<link href="css/style.css" rel="stylesheet" type="text/css" />
+		<!--		custom CSS-->
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
 
-<!--		JQuery-->
+		<!--		JQuery-->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 	</head>
 	<body>
-		<header class="head">
-			<h1>Skoolies</h1>
-		</header>
-		<p class="lead">My skoolie is a 2001 International 3000, It's 38ft long. I bought it in Feburary 2015 for $3500, and started converting it immediately. I finished in Feburauy 2016 and now have a awesome home. I created this website to display some videos and pictures I took along the way. I tried to document the entire conversion, but toward the end of the conversion I got very busy and was'nt able to take as many videos. I hope you enjoy my web site, These videos are all on youtube where you can like and comment on them.</p>
-		<img src="" alt="" />
-		<div id="bus">
-			<img src="images/finishedskooliebus.jpg" alt="Picture of my bus finished" />
+		<div class="container-fluid">
+			<div class="row">
+				<header class="head">
+					<h1>Skoolies</h1>
+				</header>
+			</div>
 		</div>
-		<div class="embed-responsive embed-responsive-16by9">
-			<iframe class="embed-responsive-item" src="https://www.youtube.com/watch?v=hw6v9Q_nA9k"></iframe>
+		<div class="container">
+			<div class="row">
+				<p class="lead">My skoolie is a 2001 International 3000, It's 38ft long. I bought it in Feburary 2015 for
+					$3500, and started converting it immediately. I finished in Feburauy 2016 and now have a awesome home. I
+					created this website to display some videos and pictures I took along the way. I tried to document the
+					entire conversion, but toward the end of the conversion I got very busy and was'nt able to take as many
+					videos. I hope you enjoy my web site, These videos are all on youtube where you can like and comment on
+					them.</p>
+			</div>
 		</div>
 
+		<!--		<img src="" alt="" />-->
+		<!--		<div id="bus">-->
+		<!--			<img src="images/finishedskooliebus.jpg" alt="Picture of my bus finished" />-->
+		<!--		</div>-->
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="embed-responsive embed-responsive-4by3">
+						<iframe class="embed-responsive-item" width="560" height="315"
+								  src="https://www.youtube.com/embed/hw6v9Q_nA9k" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--		<iframe width="560" height="315" src="https://www.youtube.com/embed/hw6v9Q_nA9k" frameborder="0" allowfullscreen></iframe>-->
 	</body>
 </html>
